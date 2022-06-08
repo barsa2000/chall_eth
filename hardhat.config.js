60ad64b26257eb4c690f3010bf05f562a36cfbea
barsa2000 <barsanti-filippo2@hotmail.it>
2022-06-08 16:48:56 +0200

    Added hardhat and yarn configs

---
+++ hardhat.config.js
@@ -0,0 +1,8 @@
+require("@nomiclabs/hardhat-waffle");
+require('@openzeppelin/hardhat-upgrades');
+require('hardhat-dependency-compiler');
+
+module.exports = {
+  networks : {hardhat : {allowUnlimitedContractSize : true}},
+  solidity : {compilers : [ {version : "0.8.7"} ]},
+}
